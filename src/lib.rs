69e2b23bbc1729060dfc6195db15eb1fbc31e820
oxalica <oxalicc@pm.me>
2024-01-02 20:16:55 +0800

    Generalize

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,38 @@
+#![allow(incomplete_features)]
+#![feature(generic_const_exprs)]
 use std::fmt;
 
 type Mask = u8;
 
-const UNIT_SIZE: usize = 1;
-const HEIGHT: u8 = 3;
-const LEAF_LEN: usize = 1 << HEIGHT;
-const TREE_LEN: usize = LEAF_LEN * 2;
-const MAX_SIZE: usize = UNIT_SIZE * LEAF_LEN;
-
 const USED: Mask = 0x80;
 
 #[derive(Debug)]
-pub struct SegTreeAlloc {
-    tree: [Mask; TREE_LEN],
+pub struct SegTreeAlloc<const U: usize, const H: u8>
+where
+    [(); 1 << (H + 1)]:,
+{
+    tree: [Mask; 1 << (H + 1)],
 }
 
-impl SegTreeAlloc {
+impl<const U: usize, const H: u8> SegTreeAlloc<U, H>
+where
+    [(); 1 << (H + 1)]:,
+{
+    const LEAF_LEN: usize = 1 << H;
+    const MAX_SIZE: usize = U * Self::LEAF_LEN;
+
     pub const fn new() -> Self {
         Self {
-            tree: [0u8; TREE_LEN],
+            tree: [0u8; 1 << (H + 1)],
         }
     }
 
     fn lvl_for_size(size: usize) -> Result<u8, ()> {
         let size = size
             .checked_next_power_of_two()
-            .filter(|&s| s < MAX_SIZE)
+            .filter(|&s| s < Self::MAX_SIZE)
             .ok_or(())?;
-        Ok(HEIGHT - (size / UNIT_SIZE).trailing_zeros() as u8)
+        Ok(H - (size / U).max(1).trailing_zeros() as u8)
     }
 
     #[allow(clippy::result_unit_err)]
@@ -45,7 +50,7 @@ impl SegTreeAlloc {
         }
         // offset = (i - 2^lvl) * (2^H / 2^lvl) * U
         //        = (i * 2^(H - lvl) - 2^H) * U
-        let off = ((i << (HEIGHT - lvl)) - (1 << HEIGHT)) * UNIT_SIZE;
+        let off = ((i << (H - lvl)) - (1 << H)) * U;
         self.tree[i] = USED;
         self.push_up(i);
         Ok(off)
@@ -56,7 +61,7 @@ impl SegTreeAlloc {
         // index = 2^lvl + offset / U / (2^H / 2^lvl)
         //       = 2^lvl + offset / U / 2^(H-lvl)
         //       = (2^H + offset / U) / 2^(H-lvl)
-        let i = ((1 << HEIGHT) + off / UNIT_SIZE) >> (HEIGHT - lvl);
+        let i = ((1 << H) + off / U) >> (H - lvl);
         debug_assert_eq!(self.tree[i], USED);
         self.tree[i] = 0;
         self.push_up(i);
@@ -72,12 +77,15 @@ impl SegTreeAlloc {
     }
 }
 
-impl fmt::Display for SegTreeAlloc {
+impl<const U: usize, const H: u8> fmt::Display for SegTreeAlloc<U, H>
+where
+    [(); 1 << (H + 1)]:,
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for lvl in 0..=HEIGHT {
+        for lvl in 0..=H {
             let off = 1 << lvl;
             let cnt = 1 << lvl;
-            let stride = LEAF_LEN / cnt;
+            let stride = Self::LEAF_LEN / cnt;
             for (i, x) in self.tree[off..][..cnt].iter().enumerate() {
                 if i != 0 {
                     write!(f, "{:len$}", "", len = (stride - 1) * 4)?;
@@ -101,7 +109,7 @@ mod tests {
 
     #[test]
     fn simple() {
-        let mut heap = SegTreeAlloc::new();
+        let mut heap = <SegTreeAlloc<1, 3>>::new();
 
         assert_eq!(heap.alloc(1).unwrap(), 0);
         assert_eq!(heap.alloc(2).unwrap(), 2);
@@ -119,8 +127,21 @@ mod tests {
     }
 
     #[test]
+    fn unit() {
+        let mut heap = <SegTreeAlloc<8, 3>>::new();
+        assert_eq!(heap.alloc(1).unwrap(), 0);
+        assert_eq!(heap.alloc(7).unwrap(), 8);
+        assert_eq!(heap.alloc(8).unwrap(), 16);
+        assert_eq!(heap.alloc(9).unwrap(), 32);
+        assert_eq!(heap.alloc(1).unwrap(), 24);
+        heap.dealloc(0, 1);
+        heap.dealloc(8, 7);
+        assert_eq!(heap.alloc(16).unwrap(), 0);
+    }
+
+    #[test]
     fn cross() {
-        let mut heap = SegTreeAlloc::new();
+        let mut heap = <SegTreeAlloc<1, 3>>::new();
         assert_eq!(heap.alloc(2).unwrap(), 0);
         assert_eq!(heap.alloc(2).unwrap(), 2);
         assert_eq!(heap.alloc(2).unwrap(), 4);
@@ -135,18 +156,20 @@ mod tests {
         const SEED: u64 = 0x6868_4242_DEAD_BEEF;
         const ROUND: usize = 1_000_000;
 
+        type Heap = SegTreeAlloc<4, 10>;
+
         let mut rng = StdRng::seed_from_u64(SEED);
-        let mut heap = SegTreeAlloc::new();
+        let mut heap = Heap::new();
         let mut alloc_map = BTreeMap::new();
         let mut alloc_idx = Vec::new();
         let mut total_allocated = 0usize;
 
-        let mut success_cnt = [(0u32, 0u32); MAX_SIZE + 1];
+        let mut success_cnt = [(0u32, 0u32); Heap::MAX_SIZE + 1];
 
         for _ in 0..ROUND {
-            let rest = MAX_SIZE - total_allocated;
+            let rest = Heap::MAX_SIZE - total_allocated;
             if rest != 0 && (alloc_map.is_empty() || rng.gen()) {
-                let size = rng.gen_range(1..=rest.min(MAX_SIZE / 2));
+                let size = rng.gen_range(1..=rest.min(Heap::MAX_SIZE / 2));
 
                 success_cnt[rest].0 += 1;
                 if let Ok(off) = heap.alloc(size) {
@@ -189,7 +212,7 @@ mod tests {
             if total != 0 {
                 println!(
                     "free={:.3} success={:.4}",
-                    size as f32 / MAX_SIZE as f32,
+                    size as f32 / Heap::MAX_SIZE as f32,
                     success as f32 / total as f32
                 );
             }
