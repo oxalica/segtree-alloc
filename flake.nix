ed086aa0d2fe158df79ba92c85d29158104aa5b5
oxalica <oxalicc@pm.me>
2024-01-02 19:51:06 +0800

    Init nightly toolchain

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs lib.systems.flakeExposed;
+    in {
+      devShells = eachSystem (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          nativeBuildInputs = [
+            rust-overlay.packages.${system}.rust-nightly_2024-01-01
+          ];
+        };
+      });
+    };
+}
